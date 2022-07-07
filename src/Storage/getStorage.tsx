758a4f938416df5aadd99f2c20a787c7c1c98094
Aliaksandr Kot <alven81.ak@gmail.com>
2022-07-07 16:40:20 +0300

    init

--- src/Storage/getStorage.tsx
+++
@@ -1,9 +0,0 @@
-const getStorage = () => {
-    if (localStorage.getItem('todoItemText')) {
-        return JSON.parse(localStorage.getItem('todoItemText') || "");
-    } else {
-        return []
-    }
-}
-
-export default getStorage
--- src/types/todolist.tsx
+++
@@ -1,7 +0,0 @@
-export default interface ITodo {
-    id: number;
-    date: string;
-    name: string;
-    description: string;
-    status: boolean
-}
